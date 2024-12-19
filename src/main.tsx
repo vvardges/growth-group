711d457e202d1cf71a64df6c61eacdcfbad55977
v.vardanyan <vako3008@gmail.com>
2024-12-19 21:16:50 +0400

    implement fetch functionality from Pexels

--- src/main.tsx
+++ src/main.tsx
@@ -1,8 +1,8 @@
 import React, { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
 
-import './index.css';
-import App from './App';
+import '@/index.css';
+import App from '@/App';
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
