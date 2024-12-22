e908b8235ccc0120700be7fa00e8dabd1a77fc2c
v.vardanyan <vako3008@gmail.com>
2024-12-23 00:43:39 +0400

    check performance

--- vite.config.ts
+++ vite.config.ts
@@ -9,14 +9,23 @@ export default defineConfig({
     rollupOptions: {
       output: {
         manualChunks(id) {
+          // Check for individual libraries and assign them to specific chunks
+          if (id.includes('node_modules/react')) {
+            return 'react'; // Separate React-related libraries into a 'react' chunk
+          }
+          if (id.includes('node_modules/lodash-es')) {
+            return 'lodash'; // Separate lodash-es into a 'lodash' chunk
+          }
+          if (id.includes('node_modules/react-router-dom')) {
+            return 'router'; // Separate react-router-dom into a 'router' chunk
+          }
+          if (id.includes('node_modules/styled-components')) {
+            return 'styled-components'; // Separate styled-components into a 'styled-components' chunk
+          }
+
+          // If none of the above conditions are matched, bundle in the vendor chunk
           if (id.includes('node_modules')) {
-            if (id.includes('lodash-es')) {
-              return 'lodash-es-vendor';
-            }
-            if (id.includes('react-dom')) {
-              return 'react-dom';
-            }
-            return 'vendor';
+            return 'vendor'; // All other third-party dependencies
           }
         },
       },
